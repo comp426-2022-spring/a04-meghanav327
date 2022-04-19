56bb893b2ae058f3fa4c4da00c805727610ff870
meghanav327 <meghanav@live.unc.edu>
2022-04-19 18:08:19 -0400

    updates

--- server.js
+++ server.js
@@ -1,9 +1,39 @@
-//const express = require('express');
-const app = express();
+// Require minimist module
+const args = require('minimist')(process.argv.slice(2))
+    // See what is stored in the object produced by minimist
+console.log(args)
+    // Store help text 
+const help = (`
+server.js [options]
 
-const args = minimist(process.argv.slice(2))
+--port	Set the port number for the server to listen on. Must be an integer
+            between 1 and 65535.
+
+--debug	If set to true, creates endlpoints /app/log/access/ which returns
+            a JSON access log from the database and /app/error which throws 
+            an error with the message "Error test successful." Defaults to 
+            false.
+
+--log		If set to false, no log files are written. Defaults to true.
+            Logs are always written to database.
+
+--help	Return this message and exit.
+`)
+    // If --help or -h, echo help text to STDOUT and exit
+if (args.help || args.h) {
+    console.log(help)
+    process.exit(0)
+}
+
 args['port']
 const port = args.port || process.env.PORT || 5000
+
+const express = require('express')
+const fs = require('fs')
+const morgan = require('morgan')
+
+const app = express();
+app.use(morgan('tiny'));
 
 const server = app.listen(port, () => {
     console.log('App listening on port %PORT%'.replace('%PORT%', port))
