b70b758321a22dfff112f7859e8ae5043eab54c4
meghanav327 <meghanav@live.unc.edu>
2022-04-30 17:39:11 -0400

    debugging

--- database.js
+++ database.js
@@ -1,32 +1,32 @@
 "use strict";
 
-const database = require('better-sqlite3');
+const database = require('better-sqlite3')
+
 const db = new database('log.db')
 
-const stmt = db.prepare(`SELECT name FROM sqlite_master WHERE type='
-    table ' and name='
-    accesslog ';`);
-
+const stmt = db.prepare(`SELECT name FROM sqlite_master WHERE type='table' and name='accesslog';`)
 let row = stmt.get();
-if (row == undefined) {
-    console.log('The database appears to be  empty. I will create it now.');
+if (row === undefined) {
+    console.log('Log database appears to be empty. Creating log database...')
+
     const sqlInit = `
-    CREATE TABLE accesslog(
-        id INTEGER PRIMARY KEY,
-        remoteaddr VARCHAR,
-        remoteuser VARCHAR,
-        time NUMBER,
-        method VARCHAR,
-        url VARCHAR,
-        protocol VARCHAR,
-        httpversion VARCHAR,
-        status INTEGER,
-        referrer VARCHAR,
-        useragent VARCHAR
-    ); `
-    db.exec(sqlInit);
-    console.log("Database created");
+        CREATE TABLE accesslog ( 
+            id INTEGER PRIMARY KEY, 
+            remoteaddr TEXT,
+            remoteuser TEXT,
+            time TEXT,
+            method TEXT,
+            url TEXT,
+            protocol TEXT,
+            httpversion TEXT,
+            status TEXT, 
+            referrer TEXT,
+            useragent TEXT
+        );
+    `
+
+    db.exec(sqlInit)
 } else {
-    console.log('Database here')
+    console.log('Log database exists.')
 }
-module.exports = db;
+module.exports = db
--- server.js
+++ server.js
@@ -1,52 +1,46 @@
-// Require minimist module
 const args = require('minimist')(process.argv.slice(2))
-    // See what is stored in the object produced by minimist
-console.log(args)
-    // Store help text 
+
 const help = (`
 server.js [options]
-
---port	Set the port number for the server to listen on. Must be an integer
+--port, -p	Set the port number for the server to listen on. Must be an integer
             between 1 and 65535.
-
---debug	If set to true, creates endlpoints /app/log/access/ which returns
+--debug, -d If set to true, creates endlpoints /app/log/access/ which returns
             a JSON access log from the database and /app/error which throws 
             an error with the message "Error test successful." Defaults to 
             false.
-
 --log		If set to false, no log files are written. Defaults to true.
             Logs are always written to database.
-
---help	Return this message and exit.
+--help, -h	Return this message and exit.
 `)
-    // If --help or -h, echo help text to STDOUT and exit
+
 if (args.help || args.h) {
     console.log(help)
     process.exit(0)
 }
 
-var express = require('express');
-const fs = require('fs');
-const morgan = require('morgan');
+var express = require("express")
+var app = express()
 
-const db = require('./database.js');
+const fs = require('fs')
 
-const port = args.port || process.env.PORT || 5000
+const morgan = require('morgan')
 
-var app = express();
+const db = require('./database.js')
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const port = args.port || args.p || 5000
 
 const server = app.listen(port, () => {
-    console.log('App listening on port %PORT%'.replace('%PORT%', port))
+    console.log("Server running on port %PORT%".replace("%PORT%", port))
 });
 
 if (args.log == 'false') {
-    console.log("Error: not creating the log file")
+    console.log("NOTICE: not creating file access.log")
 } else {
-    const WRITESTREAM = fs.createWriteStream('FILE', { flags: 'a' })
-    app.use(morgan('FORMAT', { stream: WRITESTREAM }))
+    const accessLog = fs.createWriteStream('access.log', { flags: 'a' })
+    app.use(morgan('combined', { stream: accessLog }))
 }
 
 app.use((req, res, next) => {
@@ -59,31 +53,15 @@ app.use((req, res, next) => {
         protocol: req.protocol,
         httpversion: req.httpVersion,
         status: res.statusCode,
-        referer: req.headers['referer'],
+        referrer: req.headers['referer'],
         useragent: req.headers['user-agent']
     };
     console.log(logdata)
-    const stmt = db.prepare(
-        "INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
-    );
-    const info = stmt.run(
-        logdata.remoteaddr,
-        logdata.remoteuser,
-        logdata.time,
-        logdata.method,
-        logdata.url,
-        logdata.protocol,
-        logdata.httpversion,
-        logdata.status,
-        logdata.referer,
-        logdata.useragent
-    );
+    const stmt = db.prepare('INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referrer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
+    const info = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referrer, logdata.useragent)
     next();
-});
+})
 
-
-
-// coin code
 
 function coinFlip() {
     var flip = Math.random();
@@ -131,11 +109,10 @@ function flipACoin(call) {
     return { call: call, flip: flip, result: result }
 }
 
-app.get('/app/', (req, res) => {
-    res.statusCode = 200;
-    res.statusMessage = 'OK';
-    res.writeHead(res.statusCode, { 'Content-Type': 'text/plain' });
-    res.end(res.statusCode + ' ' + res.statusMessage)
+
+app.get("/app/", (req, res, next) => {
+    res.json({ "message": "Your API works! (200)" });
+    res.status(200);
 });
 
 app.get('/app/flip', (req, res) => {
@@ -164,17 +141,28 @@ app.get('/app/flip/call/tails/', (req, res) => {
     res.json(flipACoin('tails'));
 });
 
+
 if (args.debug || args.d) {
-    app.get('/app/log/access/', (req, res) => {
+    app.get('/app/log/access/', (req, res, next) => {
         const stmt = db.prepare("SELECT * FROM accesslog").all();
         res.status(200).json(stmt);
     })
 
-    app.get('/app/error/', (req, res) => {
-        throw new Error('Error test is successful.')
+    app.get('/app/error/', (req, res, next) => {
+        throw new Error('Error test works.')
     })
 }
 
+
 app.use(function(req, res) {
-    res.status(404).send('404 NOT FOUND');
+    const statusCode = 404
+    const statusMessage = 'NOT FOUND'
+    res.status(statusCode).end(statusCode + ' ' + statusMessage)
+});
+
+
+process.on('SIGINT', () => {
+    server.close(() => {
+        console.log('\nApp stopped.');
+    });
 });
